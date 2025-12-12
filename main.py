5f4ec1026558ce42aa624b5b095fb63f94977181
otvintadkt <otvintadkt>
2025-12-12 15:13:14 +0300

    Added check for left hand. Returned check for straight hand

--- main.py
+++ main.py
@@ -59,31 +59,54 @@ with mp_pose.Pose(
                 results.pose_landmarks,
                 mp_pose.POSE_CONNECTIONS
             )
-            x1 = results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_SHOULDER].x
-            y1 = results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_SHOULDER].y
-            x2 = results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_ELBOW].x
-            y2 = results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_ELBOW].y
-            x3 = results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_WRIST].x
-            y3 = results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_WRIST].y
-            vector1 = Vector(x1, y1, x2, y2)
-            vector2 = Vector(x2, y2, x3, y3)
+            x_right1 = results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_SHOULDER].x
+            y_right1 = results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_SHOULDER].y
+            x_right2 = results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_ELBOW].x
+            y_right2 = results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_ELBOW].y
+            x_right3 = results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_WRIST].x
+            y_right3 = results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_WRIST].y
+            vector_rigth1 = Vector(x_right1, y_right1, x_right2, y_right2)
+            vector_right2 = Vector(x_right2, y_right2, x_right3, y_right3)
+
+            x_left1 = results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_SHOULDER].x
+            y_left1 = results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_SHOULDER].y
+            x_left2 = results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_ELBOW].x
+            y_left2 = results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_ELBOW].y
+            x_left3 = results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_WRIST].x
+            y_left3 = results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_WRIST].y
+            vector_left1 = Vector(x_left1, y_left1, x_left2, y_left2)
+            vector_left2 = Vector(x_left2, y_left2, x_left3, y_left3)
             # if is_straight(vector1, vector2):
             #     print("straight")
             # else:
             #     print("blocked")
 
-            vector_hand = Vector(x1, y1, x3, y3)
-            azimuth = cnt_angle(vector_hand, vector_north)
-            azimuth = (azimuth + 360) % 360
-            direction = ""
-            if azimuth < 45 or azimuth > 315: direction = "north"
-            elif azimuth < 135: direction = "east"
-            elif azimuth < 225: direction = "south"
-            else: direction = "west"
+            vector_right_hand = Vector(x_right1, y_right1, x_right3, y_right3)
+            vector_left_hand = Vector(x_left2, y_left2, x_left3, y_left3)
+            azimuth_right = cnt_angle(vector_right_hand, vector_north)
+            azimuth_left = cnt_angle(vector_left_hand, vector_north)
+            azimuth_right = (azimuth_right + 360) % 360
+            azimuth_left = (azimuth_left + 360) % 360
+            direction_right = "wrong"
+            direction_left = "wrong"
+
+            if is_straight(vector_left1, vector_left2):
+                if azimuth_right < 45 or azimuth_right > 315: direction_right = "north"
+                elif azimuth_right < 135: direction_right = "east"
+                elif azimuth_right < 225: direction_right = "south"
+                else: direction_right = "west"
+
+            if is_straight(vector_rigth1, vector_right2):
+                if azimuth_left < 45 or azimuth_left > 315: direction_left = "north"
+                elif azimuth_left < 135: direction_left = "east"
+                elif azimuth_left < 225: direction_left = "south"
+                else: direction_left = "west"
 
             text_cord = (50, 50)
-            cv2.putText(frame, direction, text_cord, cv2.FONT_HERSHEY_PLAIN, 3, (0, 0, 0), 4)
-            print("angle:", (azimuth + 360) % 360)
+            cv2.putText(frame, direction_right, text_cord, cv2.FONT_HERSHEY_PLAIN, 3, (0, 0, 0), 4)
+            cv2.putText(frame, direction_left, (text_cord[0], text_cord[1] + 80 ), cv2.FONT_HERSHEY_PLAIN, 3, (0, 0, 0), 4)
+            print("angle_right:", azimuth_right)
+            print("angle_left:", azimuth_left)
 
         cv2.imshow("", frame)
 
